50b80c476ffd435c1ecc0f0758c7594ea4af54ed
nanand.r <rafirenan12@gmail.com>
2025-07-17 18:57:18 +0700

    generate model and controller

---
+++ app/Models/FormatSurat.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FormatSurat extends Model
+{
+    protected $table = "format_surat";
+}
---
+++ app/Models/Lapak.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lapak extends Model
+{
+    protected $table = "lapak";
+}
